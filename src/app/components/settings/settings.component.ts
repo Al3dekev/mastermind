f7048dfd8ab9cae56d28313c03a3f5a9cfbe14e7
Al3dekev <quentin1998@me.com>
2020-02-06 19:10:45 +0100

    Code: Some security & test options added to the settingsComponent

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -8,12 +8,22 @@ import {Router} from '@angular/router';
 })
 export class SettingsComponent implements OnInit {
 
+  @ViewChild('inputTurns', {static: false}) inputTurn: ElementRef;
+  private testMod = true;
+
   constructor(private router: Router) { }
 
   accessGame() {
-    this.router.navigate(['/game']).then(r =>
-      console.log(r)
-    );
+    if (this.testMod) {
+      this.inputTurn.nativeElement.valueOf().value = '10';
+    }
+    if (this.inputTurn.nativeElement.valueOf().value === '') {
+      alert('Please insert a turn number');
+    } else {
+      this.router.navigate(['/game']).then(r =>
+        console.log(r)
+      );
+    }
   }
 
   ngOnInit() {
