e471ebaeebe0060dfed6d7b4eb654a8b9c5d44be
Al3dekev <quentin1998@me.com>
2020-02-09 16:05:17 +0100

    Code: First Result Grid update to start working about the huge result grid update that will make everything works. Validate btn still need some works.

--- src/app/components/result-grid/result-grid.component.ts
+++ src/app/components/result-grid/result-grid.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {ManagerService} from '../../services/manager.service';
+import {GridSystem} from '../../GridSystem';
 
 @Component({
   selector: 'app-result-grid',
   templateUrl: './result-grid.component.html',
   styleUrls: ['./result-grid.component.css']
 })
-export class ResultGridComponent implements OnInit {
+export class ResultGridComponent extends GridSystem implements OnInit {
 
-  constructor() { }
+  constructor(public ms: ManagerService) {
+    super(ms);
+  }
 
   ngOnInit() {
   }
